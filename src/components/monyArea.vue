a171dc631dcb39ebf97999aa75d38cb9f1cacda5
RafaelMurrieta <r7121005846@gmail.com>
2024-03-30 23:17:27 -0600

    Elementos de banco

--- src/components/monyArea.vue
+++ src/components/monyArea.vue
@@ -1,13 +1,37 @@
 <template>
     <div class="container-money">
-        <div class="smallchange">sadsad</div>
-        <div class="money">dsad</div>
-        sjdiajisadhaih
+        <div class="smallchange">
+            <img src="../assets/images/moneyTwo.svg">
+        </div>
+        <div class="money">
+            <h1>snajxn</h1>
+        </div>
     </div>
 </template>
 
 <style>
-
+.container-money{
+    display: grid;
+}
+.smallchange{
+    display: flex;
+    justify-content: right;
+    height: 80%;
+    align-items: start;
+}
+.smallchange > img{
+    position:absolute;
+    width: 7%;
+}
+.money{
+    display: grid;
+    text-align: center;
+}
+.money h1{
+    margin-left: 10%;
+    position: absolute;
+    font-size: 2rem;
+}
 </style>
 
 <script>
